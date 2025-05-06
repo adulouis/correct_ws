b9caf839efc6ae970532c1da0be674a86aacc576
adulouis <adulouis980@gmail.com>
2025-05-06 00:40:34 +0000

    creating odometry_motion_model c++

---
+++ src/correct_localization/include/correct_localization/odometry_motion_model.hpp
@@ -0,0 +1,32 @@
+#ifndef ODOMETRY_MOTION_MODEL_HPP
+#define ODOMETRY_MOTION_MODEL_HPP
+#include <rclcpp/rclcpp.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/pose_array.hpp>
+
+class OdometryMotionModel : public rclcpp::Node
+{
+    public:
+        OdometryMotionModel(const std::string &name);
+
+
+    private:
+
+        void odomCallback(const nav_msgs::msg::Odometry &odom);
+
+        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub;
+        rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr pose_array_pub;
+
+        geometry_msgs::msg::PoseArray samples;
+        double alpha1;
+        double alpha1;
+        double alpha1;
+        double alpha1;
+        int nr_samples;
+        bool is_first_odom;
+        double last_odom_x;
+        double last_odom_y;
+        double last_odom_theta;
+};
+
+#endif // ODOMETRY_MOTION_MODEL_HPP

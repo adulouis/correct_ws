7945bdba863ec4584bfd9750ab20d083c13a3713
adulouis <adulouis980@gmail.com>
2025-06-07 19:26:22 +0000

    updated safety_stop.cpp

--- src/correct_utils/src/safety_stop.cpp
+++ src/correct_utils/src/safety_stop.cpp
@@ -4,6 +4,7 @@
 #include "math.h"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "twist_mux_msgs/action/joy_turbo.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
 
 
 //To keep track of the robot we give a free, warning or danger state
@@ -28,10 +29,13 @@ class SafetyStop : public rclcpp::Node
             scan_topic = get_parameter("scan_topic").as_string();
             safety_stop_topic = get_parameter("safety_stop_topic").as_string();
 
+            is_first_msg = true;
+
             laser_sub = this->create_subscription<sensor_msgs::msg::LaserScan>(scan_topic, 10,
             std::bind(&SafetyStop::laserCallback, this, std::placeholders::_1));
 
             safety_stop_pub = this->create_publisher<std_msgs::msg::Bool>(safety_stop_topic, 10);
+            zones_pub = this->create_publisher<visualization_msgs::msg::MarkerArray>("zones",10);
 
             decrease_speed_client = rclcpp_action::create_client<twist_mux_msgs::action::JoyTurbo>(this, "joy_turbo_decrease");
             increase_speed_client = rclcpp_action::create_client<twist_mux_msgs::action::JoyTurbo>(this, "joy_turbo_increase");
@@ -45,18 +49,44 @@ class SafetyStop : public rclcpp::Node
                 RCLCPP_WARN(get_logger(),"Action /joy_turbo_increase not available! waiting...");
                 std::this_thread::sleep_for(std::chrono::seconds(1));
             }
+
+            visualization_msgs::msg::Marker warning_zone;
+            warning_zone.id = 0;
+            warning_zone.type = visualization_msgs::msg::Marker::CYLINDER;
+            warning_zone.action = visualization_msgs::msg::Marker::ADD;
+            warning_zone.scale.z = 0.001;
+            warning_zone.scale.x = warning_distance * 2;
+            warning_zone.scale.y = warning_distance * 2;
+            warning_zone.color.r = 1.0;
+            warning_zone.color.g = 0.984;
+            warning_zone.color.b = 0.0;
+            warning_zone.color.a = 0.5;
+            zones.markers.push_back(warning_zone);
+            visualization_msgs::msg::Marker danger_zone = warning_zone;
+            danger_zone.id = 1;
+            danger_zone.scale.x = danger_distance * 2;
+            danger_zone.scale.y = danger_distance * 2;
+            danger_zone.color.r = 1.0;
+            danger_zone.color.g = 0.0;
+            danger_zone.color.b = 0.0;
+            danger_zone.color.a = 0.5;
+            danger_zone.pose.position.z = 0.001;
+            zones.markers.push_back(danger_zone);
         }
         
         
     private:
+        bool is_first_msg;
         double danger_distance, warning_distance;
         std::string scan_topic;
         std::string safety_stop_topic;
         rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub;
         rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr safety_stop_pub;
+        rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr zones_pub;
         rclcpp_action::Client<twist_mux_msgs::action::JoyTurbo>::SharedPtr decrease_speed_client;
         rclcpp_action::Client<twist_mux_msgs::action::JoyTurbo>::SharedPtr increase_speed_client; 
         State state, prev_state;
+        visualization_msgs::msg::MarkerArray zones;
 
         void laserCallback(const sensor_msgs::msg::LaserScan &msg)
         {
@@ -80,14 +110,20 @@ class SafetyStop : public rclcpp::Node
                 if(state == State::WARNING)
                 {
                     is_safet_stop.data = false;
+                    zones.markers[0].color.a = 1.0;
+                    zones.markers[1].color.a = 0.5;
                     decrease_speed_client->async_send_goal(twist_mux_msgs::action::JoyTurbo::Goal());
                 }
                 else if(state == State::DANGER)
                 {
+                    zones.markers[0].color.a = 1.0;
+                    zones.markers[1].color.a = 1.0;
                     is_safet_stop.data = true;
                 }
                 else if(state == State::FREE)
                 {
+                    zones.markers[0].color.a = 0.5;
+                    zones.markers[0].color.a = 0.5;
                     is_safet_stop.data = false;
                     increase_speed_client->async_send_goal(twist_mux_msgs::action::JoyTurbo::Goal());
 
@@ -95,6 +131,16 @@ class SafetyStop : public rclcpp::Node
                 prev_state = state;
                 safety_stop_pub->publish(is_safet_stop);
             }
+
+            if(is_first_msg)
+            {
+                for(auto &zone: zones.markers)
+                {
+                    zone.header.frame_id = msg.header.frame_id;
+                }
+                is_first_msg = false;
+            }
+            zones_pub->publish(zones);
             
         }
 };
